ac137c8e318d936eb720a2445bad3162180ad22c
mrdrath <hi.mr.drath@gmail.com>
2023-04-17 16:35:14 +0530

    Project Updated

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.use(morgan('dev'));
 
 mongoose.connect(
   dburi,
-  {  useCreateIndex: true },
+  { useNewUrlParser: true, useCreateIndex: true },
   (err) => {
     if (err) throw err;
     else console.log('Connected to mongoDb');
@@ -193,7 +193,7 @@ app.get('/logout', (req, res) => {
   res.redirect('/');
 });
 
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 27017;
 app.listen(port, () => {
   console.log('App listening on port ' + port + '!');
 });

9e0b094c8c74f9e8e848c098633c7eb85377bec0
mrdrath <hi.mr.drath@gmail.com>
2023-04-17 18:28:17 +0530

    Project Updated

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ var mongoose = require('mongoose');
 var userModel = require('./models/user');
 
 const KEY = process.env.KEY;
-const dburi = process.env.DBURI||'development';
+const dburi = process.env.DBURI;
 const signature = {
   signed: KEY,
   maxAge: 2 * 24 * 60 * 60 * 1000,
